8acdeb9afd0a2c4ce6951a93728dd9c0edaa99cf
zaemon1251 <heste.m@icloud.com>
2021-02-09 19:39:22 +0900

    database

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -137,7 +137,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
-#DATABASES['default'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 from .settings_common import*
 
