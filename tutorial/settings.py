c85d4dba6ab92f27cffbbef7c7742c3755ad38ed
zaemon1251 <heste.m@icloud.com>
2021-02-09 22:51:08 +0900

    change account

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -78,12 +78,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'hisakawa1251@gmail.com'
+EMAIL_HOST_USER = 'supergbzx@gmail.com'
 EMAIL_HOST_PASSWORD = os.getenv("GMAIL_PASS")
-SERVER_EMAIL = 'hisakawa1251@gmail.com'
+SERVER_EMAIL = 'supergbzx@gmail.com'
 
 ADMINS = [
-        ('me', 'hisakawa1251@gmail.com'),
+        ('me', 'supergbzx@gmail.com'),
 ]
 
 ALLOWED_HOSTS = ['*','.herokuapp.com']
