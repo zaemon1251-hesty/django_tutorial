68e21aada6d1ae6bce566e4ec77631748b05422f
zaemon1251-hesty <hisakawa1251@gmail.com>
2021-11-05 12:49:31 +0900

    fix: delpy when change contents

--- cms/models.py
+++ cms/models.py
@@ -1,6 +1,10 @@
 from django.db import models
-
+from django.db.models.signals import post_delete, post_save
+import urllib.request
+import os
 # Create your models here.
+
+     
 
 class Product(models.Model):
     """成果物"""
@@ -40,3 +44,10 @@ class BlogPost(models.Model):
     
     def __str__(self):
         return self.title
+
+def after_crud(sender, instance, *args, **kwargs):
+    urllib.request.Request(os.getenv("DEPLY_HOOK_URL"), method='POST')
+    print(sender)
+for model in [Product, Article, BlogPost]:
+    post_save.connect(after_crud, sender=model)
+    post_delete.connect(after_crud, sender=model)
