84a204a2a6a372dbf10a225c6f8770bb29962dd6
zaemon1251-hesty <hisakawa1251@gmail.com>
2022-05-26 20:01:45 +0900

    add:  markdown extensions

--- cms/service.py
+++ cms/service.py
@@ -1,5 +1,8 @@
 import markdown
 
+# markdown extension configuration
+md_extensions = ['abbr', 'fenced_code', 'tables']
+
 
 def get_html_from_text(text: str) -> str:
     """get html from text
@@ -10,4 +13,6 @@ def get_html_from_text(text: str) -> str:
     Returns:
         str: html5 format generated from the text
     """
-    return markdown.markdown(text, extensions=['fenced_code'])
+    return markdown.markdown(
+        text, extensions=md_extensions
+    )
